ad8264a66e6b927f2dc180a90adc148d6262a16f
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-03-09 18:07:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName){
+    let messages = [];
+    for(let i=0; i < names.length; i++){
+         console.log(i);
+       console.log (messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`));      
+    }
+
+    return messages;
+}
+writeCards( ["Ada", "Brendan", "Ali"],"birthday");
+
+
+
+function countDown(num){
+        while(num>=0){
+            console.log(num);
+            num -- ;
+        }
+        }
+countDown(10);
